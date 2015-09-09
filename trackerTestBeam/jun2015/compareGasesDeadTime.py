4c3d0b090dd8912a2625881cd3b42da05841feb1
Tom Stuttard <stuttard@fnal.gov>
2015-09-09 15:05:06 +0100

    Adding plots as writing presentation comparing gases

---
+++ trackerTestBeam/jun2015/compareGasesDeadTime.py
@@ -0,0 +1,47 @@
+from ROOT import TFile, TCanvas, gStyle, TH1F
+import os
+import argparse
+import RootHelper as rh
+from collections import OrderedDict
+
+#Define files to read
+inputRootDir = '/unix/muons/g-2/scratch/tom/sim/gm2Dev_v6_01_00_testbeam_merge/data/'
+inputFileName = 'mtestDigitsAnalysis_strawTriggerDigitPlots.root'
+inputFiles = OrderedDict()
+inputFiles['Ar-CO2 : Run 316'] = inputRootDir + 'run00316/' + inputFileName
+inputFiles['Ar-Ethane : Run 405'] = inputRootDir + 'run00405/' + inputFileName
+
+#Open all files (keep them in dict so have them in memory simultaneously)
+rootFiles = OrderedDict()
+for key, rootFileName in inputFiles.items() :
+  rootFiles[key] = rh.openFile(rootFileName)
+
+
+#
+# Drift time diff histo
+#
+
+#Create a fresh canvas
+canvas = TCanvas()
+canvas.Divide(2)
+#gStyle.SetOptStat(False)
+
+#Loop over files
+counter = 0
+for key, rootFile in rootFiles.items() :
+
+  #Get histogram for hottest straw hit time difference with self
+  hottestStrawDir = 'StrawDigits/Station_0/Module_0/View_0/Layer_1/Wire_11/' #Hottest straw
+  hist = rh.getFromFile(rootFile,hottestStrawDir+'h_hitTimeDiff_S0_M0_V0_L1_W11') #Hottest straw again
+  counter += 1
+
+  #Format histo
+  hist.SetTitle(key+';Hit time difference [ns];')
+  hist.GetXaxis().SetRangeUser(-200.,50.)
+
+  #Draw to canvas
+  canvas.cd(counter)
+  hist.Draw()
+
+raw_input("Press Enter to continue...")
+
---
+++ trackerTestBeam/jun2015/compareGasesEfficiency.py
@@ -0,0 +1,98 @@
+from ROOT import TFile, TCanvas, gStyle, TH1F
+import os
+import argparse
+import RootHelper as rh
+from collections import OrderedDict
+
+#Define files to read
+inputRootDir = '/unix/muons/g-2/scratch/tom/sim/gm2Dev_v6_01_00_testbeam_merge/data/'
+inputFileName = 'mtestRecoAnalysis_strawEfficiencyPlots.root'
+efficiencyScanFiles = OrderedDict()
+inputFiles = OrderedDict()
+inputFiles['Ar-CO2 : Run 316'] = inputRootDir + 'run00316/' + inputFileName
+inputFiles['Ar-Ethane : Run 405'] = inputRootDir + 'run00405/' + inputFileName
+
+#Open all files (keep them in dict so have them in memory simultaneously)
+rootFiles = OrderedDict()
+for key, rootFileName in inputFiles.iteritems() :
+  rootFiles[key] = rh.openFile(rootFileName)
+
+
+#
+# Num digits in island histo
+#
+
+#Create a fresh canvas
+canvas = TCanvas()
+canvas.Divide(2)
+#gStyle.SetOptStat(False)
+
+#Loop over files
+counter = 0
+for key, rootFile in rootFiles.items() :
+
+  #Get histogram
+  hist = rh.getFromFile(rootFile,'StrawEfficiency/h_numDigitsInIsland')
+  counter += 1
+
+  #Format histo
+  hist.SetTitle(key+';Num digits in island;')
+
+  #Draw to canvas
+  canvas.cd(counter)
+  hist.Draw()
+
+raw_input("Press Enter to continue...")
+
+
+#
+# Num seeds in island histo
+#
+
+#Create a fresh canvas
+canvas = TCanvas()
+canvas.Divide(2)
+
+#Loop over files
+counter = 0
+for key, rootFile in rootFiles.items() :
+
+  #Get histogram
+  hist = rh.getFromFile(rootFile,'StrawEfficiency/h_numSeedsFormedFromIsland')
+  counter += 1
+
+  #Format histo
+  hist.SetTitle(key+';Num seeds formed from island;')
+
+  #Draw to canvas
+  canvas.cd(counter)
+  hist.Draw()
+
+raw_input("Press Enter to continue...")
+
+
+#
+# Read/reco steps # digit summaries
+#
+
+#Create a fresh canvas
+canvas = TCanvas()
+canvas.Divide(2)
+
+#Loop over files
+counter = 0
+for key, rootFile in rootFiles.items() :
+
+  #Get histogram
+  hist = rh.getFromFile(rootFile,'StrawEfficiency/h_summary')
+  counter += 1
+
+  #Format histo
+  hist.SetTitle(key)
+  hist.GetYaxis().SetRangeUser(0.,200.e3)
+
+  #Draw to canvas
+  canvas.cd(counter)
+  hist.Draw()
+
+raw_input("Press Enter to continue...")
