bc75c0add7c5f67c5c533ddf8a33a4aae81f7e7c
Tom Stuttard <ts4051@googlemail.com>
2015-10-16 14:42:28 +0100

    Affect on reco of straw resolution model now ready to go

--- trackerTestBeam/jun2015/modelResolutionEffectOnModuleHitReco.py
+++ trackerTestBeam/jun2015/modelResolutionEffectOnModuleHitReco.py
@@ -2,188 +2,173 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
 from scipy.optimize import curve_fit
-from scipy.stats import chi2
+from scipy.stats import chi2, norm
 from sys import argv,exit
 import datetime
 from math import log10,pow,sqrt,fabs
 from random import gauss,uniform
 import argparse
+import collections 
+import math
 
-def dca(start,end,point):
+#Everything in this script is in y-z plane
+#Lines defined by z = my + c
+#All distance units in [um]
 
-    xDelta = end[0] - start[0]
-    yDelta = end[1] - start[1]
 
-    m = yDelta/xDelta
-    c  = end[1] - m*end[0]
+'''
+Helper functions and structures
+'''
 
-    if ((xDelta == 0) and (yDelta == 0)):
-      raise ValueError('dca : xDelta and yDelta are both ZERO')
+#Define structs
+Coord = collections.namedtuple('Coord', 'y z')
+Line = collections.namedtuple('Line', 'm c')
 
-    u = ( (point[0] - start[0])*xDelta + (point[1] - start[1])*yDelta ) / (xDelta*xDelta + yDelta*yDelta)
+#Get points on line
+def getLineZ(y,line):
+  z = line.m*y + line.c
+  return z
 
-    if (u < 0):
-      closestPoint = [start[0], start[1]]
-    elif (u > 1):
-      closestPoint = [end[1], end[1]]
-    else:
-      closestPoint = [ start[0]+u*xDelta,  start[1]+u*yDelta]
+def getLineY(z,line):
+  y = (z-line.c)/line.m
+  return y
 
-    dx = closestPoint[0] - point[0]
-    dy = closestPoint[1] - point[1]
-    
-    if (dy >= 0):
-      sign = -1
-    else:
-      sign = +1
+#Get line end points
+def getLineEndsFromZ(z1,z2,line):
+  zVals = [z1,z2]
+  yVals = [ getLineY(zVals[0],line) , getLineY(zVals[1],line) ]
+  return yVals,zVals
 
-    dcaV = sign*sqrt(dx*dx +dy*dy)
 
-    return dcaV
+'''
+Main
+'''
 
-def fixTime(time):
-    #if (time < 0): #TODO Do we want this?
-    #    time = fabs(time)
-    return time
-
-def straight_line_fit(z, a, b):
-  return a + b*z
-
-def gaussian_fit(x,A,mu,sigma):
-    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
-
+#Get command line args
 parser = argparse.ArgumentParser(description='Produce simple u,v drift time correlation with resolution')
-parser.add_argument('--res', action='store', dest='resolution' , type=float , default = -1., help='Set straw resolution in microns')
-parser.add_argument('--nev', action='store', dest='nevents' , type=int , default = -1, help='Num hits to generate')
+parser.add_argument('--res', action='store', dest='resolution' , type=float , default=-1., required=True, help='Set straw resolution in microns')
+parser.add_argument('--nev', action='store', dest='nevents' , type=int , default=-1, required=True, help='Num hits to generate')
+parser.add_argument('--plot', action='store_true', dest='plot')
+parser.add_argument('--verbose', action='store_true', dest='verbose')
 args = parser.parse_args()
 
 # Drift velocity: 50 microns per ns (is this true for ethane ?)
-VD = 50
-# Maxium drift time (from drift velocity and radius)
-MAXD = 2500. / VD
-
-if (args.resolution == -1):
-    print "\nERROR: --res must be set\n\n"
-    parser.print_help()
-    exit (-1)
-else:
-    resol = int(args.resolution)
-    print 'Distance resolution [um] =',resol
-
-if (args.nevents == -1):
-    print "\nERROR: --nev must be set\n\n"
-    parser.print_help()
-    exit (-1)
-else:
-    nev = int(args.nevents)
+driftVelocity = 50
 
 # Resolution, smearing to add to hit time due to resolution    
-thit = resol/VD
-print 'Time resolution (from distance resolution) [ns] =',thit
+#timeResolution = args.resolution/driftVelocity
+#print 'Time resolution (from distance resolution) [ns] =',timeResolution
 
 #finetime_smear = 0. #ns
-finetime_smear = 2.5 #ns
-print 'Fine time binning smearing [ns] =',finetime_smear
-
-#Define structs
-Pos = collections.namedtuple('Pos', 'y z')
-Line = collections.namedtuple('Line', 'm c')
+#finetime_smear = 2.5 #ns
+#print 'Fine time binning smearing [ns] =',finetime_smear
 
 #Define geom
 strawAngle = 7.5 #deg
 strawGradient = 1. / math.tan( math.radians(strawAngle) )
+strawMinZ = -42.6321e3
+strawMaxZ = 42.6321e3
 
-# Generate events
-for i in xrange(0,nev,1):    
+
+
+#Generate events
+residuals = []
+smearingVals = []
+interceptSmearingVals = []
+for i in xrange(0,args.nevents,1):    
+
+  if args.verbose: print '\nEvent',i
+
+  if args.plot: plt.title('Event %i : Truth vs reco hits (shows result of single straw resolution)' % (i) )
 
   #Get hit pos (transverse)
-  hitPos = Pos( y=gauss(0.,3.) , z=gauss(0.,3.) )
+  truthHitPos = Coord( y=gauss(0.,3.e3) , z=gauss(0.,3.e3) )
+  if args.verbose: print "Truth hit pos =",truthHitPos
+  if args.plot: plt.scatter([truthHitPos.y],[truthHitPos.z],marker='o',c='blue')
 
-  #Get ioschrone lines from hit pos
-  for i_us in range(0,2) : ##Loop over U view straws
-    isochroneLine = Line( m=strawGradient , c=hitPos.y-(strawGradient*hitPos.x) )
-    print isochroneLine
+  #Get truth ioschrone lines for each doublet from hit pos
+  doubletTruthIsochroneLines = []
+  for i_d in range(0,2) : #Loop over doublets
+    gradientSign = -1. if i_d % 2 == 0 else 1.  #One view is +ve, other -ve
+    m = gradientSign * strawGradient
+    truthIsochroneLine = Line( m=m , c=truthHitPos.z-(m*truthHitPos.y) )
+    if args.verbose: print "Truth isochrone line =",truthIsochroneLine
+    doubletTruthIsochroneLines.append( truthIsochroneLine )
+    if args.plot: #Draw it
+      lineYVals,lineZVals = getLineEndsFromZ(strawMinZ,strawMaxZ,truthIsochroneLine)
+      plt.plot(lineYVals,lineZVals,"b--")
 
-'''
-# Generate uncorrelated background
-for i in xrange(0,int(nev*bgfrac),1):  
-  
-  hitu = uniform(0.0, 50.0)
-  uhit = fixTime(gauss(hitu, finetime_smear)) #Fine time binning smear
-  uhit = fixTime(gauss(uhit, timesync_smear)) #Time sync smear
+  #The intercept of a indivudual straw isochroine line (really cylinder) changes 
+  #according to the resolution (which affects radial measurements)
+  #Make this change and see effect
+  doubletSmearedIsochroneLines = []
+  for truthIsochroneLine in doubletTruthIsochroneLines :
 
-  hitv = uniform(0.0, 50.0) #Uncorrelated
-  vhit = fixTime(gauss(hitv, finetime_smear)) #Fine time binning smear
-  vhit = fixTime(gauss(vhit, timesync_smear)) #Time sync smear
+    #Smear intercept in both straws and get mean (reco takes mean)
+    interceptSum = 0.
+    for i_s in range(0,2) :
+      smearing = gauss(0.,args.resolution)
+      if args.verbose: print "Smearing (single straw) =",smearing
+      smearingVals.append(smearing)
+      smearedIntercept = truthIsochroneLine.c + (truthIsochroneLine.m*smearing) #Smear intercept
+      interceptSmearingVals.append( smearedIntercept - truthIsochroneLine.c ) #Record residuals
+      interceptSum += smearedIntercept
+    interceptMean = interceptSum / 2.
 
-  #Only record hit if passes sum cut
-  if (MAXD-sumcut) <= (uhit+vhit) <= (MAXD+sumcut) : 
-    hitu_smear.append(uhit)
-    hitv_smear.append(vhit)
-    #print (uhit,hitv)
-  else:
-    hitu_cut.append(uhit) #Record cut hit positions too
-    hitv_cut.append(vhit)
+    #Record as new ioschrone
+    smearedIsochroneLine = Line( m=truthIsochroneLine.m , c=interceptMean )
+    if args.verbose: print "Smeared isochrone line =",smearedIsochroneLine
+    doubletSmearedIsochroneLines.append( smearedIsochroneLine )
+    if args.plot: #Draw it
+      lineYVals,lineZVals = getLineEndsFromZ(strawMinZ,strawMaxZ,smearedIsochroneLine)
+      plt.plot(lineYVals,lineZVals,"r--")
 
-if len(hitu_smear)==0 :
-  print 'No data passed cuts'
-  exit(0)
+  #Find new hit position from smeared lines
+  #  y = (c_v - c_u) / (m_u - m_v)   
+  #  z = m_u * y + c_u   (double use either view m and c)
+  yReco = ( doubletSmearedIsochroneLines[0].c - doubletSmearedIsochroneLines[1].c ) / ( doubletSmearedIsochroneLines[1].m - doubletSmearedIsochroneLines[0].m )
+  zReco = ( doubletSmearedIsochroneLines[0].m * yReco ) + doubletSmearedIsochroneLines[0].c
+  recoHitPos = Coord( y=yReco , z=zReco )
+  if args.verbose: print "Reco hit pos =",recoHitPos
+  if args.plot: plt.scatter([recoHitPos.y],[recoHitPos.z],marker='o',c='red')
 
-x = np.array(hitu_smear)  
-y = np.array(hitv_smear)  
-xcut = np.array(hitu_cut)  
-ycut = np.array(hitv_cut)  
+  #Show isochrone lines and hit positons on a plot
+  if args.plot: plt.show()
 
-# Fit these to a straight line
-popt, pcov = curve_fit(straight_line_fit, x, y)
-fitGradient = popt[1]
-fitIntercept = popt[0]
-print "Fit: gradient = %f, intercept = %f" % (fitGradient,fitIntercept)
+  #Get residuals
+  residual = Coord( y=recoHitPos.y-truthHitPos.y , z=recoHitPos.z-truthHitPos.z )
+  if args.verbose: print "Residual =",residual
+  residuals.append(residual)
 
-# Best fit line end points
-xFit = [-20.,70.]
-yFit = [ fitGradient*xFit[0]+fitIntercept , fitGradient*xFit[1]+fitIntercept ]
-
-#Plot
-plt.title('Drift times in doublet (layer0=x,layer1=y) [ns]')
-plt.scatter(x,y,marker='.',c='blue')
-plt.plot(xFit,yFit,"g-")
+#Plot the single straw smearing
+n,bins,patches = plt.hist(np.array(smearingVals),bins=100,normed=True) #Fill hist (normalise for Gaussian fitting)
+(mu, sigma) = norm.fit(smearingVals) #Fit Gaussian
+plt.plot(bins, mlab.normpdf(bins,mu,sigma), 'r--', linewidth=2) #Plot the fit
+plt.title('Single straw smearing : mean=%f sigma=%f : [um]'%(mu,sigma))
 plt.show()
 
-#Also plot with cut hits
-plt.title('Drift times in doublet (layer0=x,layer1=y) [ns]')
-plt.scatter(x,y,marker='o',c='blue')
-plt.scatter(xcut,ycut,marker='o',c='red')
-plt.plot(xFit,yFit,"g-")
+#Plot the intercept smearing
+n,bins,patches = plt.hist(np.array(interceptSmearingVals),bins=100,normed=True) #Fill hist (normalise for Gaussian fitting)
+(mu, sigma) = norm.fit(interceptSmearingVals) #Fit Gaussian
+plt.plot(bins, mlab.normpdf(bins,mu,sigma), 'r--', linewidth=2) #Plot the fit
+plt.title('Intercept smearing : mean=%f sigma=%f : [um]'%(mu,sigma))
 plt.show()
 
-
-# Loop over the hits and plot the residual (dca)
-residuals = []
-
-for i in xrange(0,len(x)):
-    xv = x[i]
-    yv = y[i]
-    point = [xv,yv]
-
-    try:
-# Residual from DCA        
-      resid = dca((xFit[0],yFit[0]),(xFit[1],yFit[1]),point)
-      residuals.append(resid)
-    except ValueError as err:
-      print(err.args)
-    
-#Plot the residuals
-RPTS = np.array(residuals) #Residuals
-mean = np.mean(RPTS)
-variance = np.var(RPTS)
-sigma = np.sqrt(variance)
-print "Residuals Gaussian: mean = %f, variance = %f, sigma = %f" % (mean,variance,sigma) 
-x = np.linspace(min(RPTS), max(RPTS),100) #Bins
-plt.title('Time residuals [ns] (normalised)')
-plt.hist(RPTS, normed=True) #Must normalise histogram for Gaussian overlay
-plt.plot(x,mlab.normpdf(x,mean,sigma),"r") #Plot bins
+#Plot the y residuals (with Gaussian fit)
+yResiduals = []  #Get list of y residuals
+for res in residuals : yResiduals.append(res.y)
+n,bins,patches = plt.hist(np.array(yResiduals),bins=100,normed=True) #Fill hist (normalise for Gaussian fitting)
+(mu, sigma) = norm.fit(yResiduals) #Fit Gaussian
+plt.plot(bins, mlab.normpdf(bins,mu,sigma), 'r--', linewidth=2) #Plot the fit
+plt.title('Fit y residuals : mean=%f sigma=%f : [um]'%(mu,sigma))
 plt.show()
-print "Residuals Mean = %f, Sigma = %f" % (mean,sigma)
-'''
+
+#Plot the z residuals (with Gaussian fit)
+zResidualsUm = []  #Get list of z residuals
+for res in residuals : zResidualsUm.append(res.z)
+n,bins,patches = plt.hist(np.array(zResidualsUm),bins=100,normed=True) #Fill hist (normalise for Gaussian fitting)
+(mu, sigma) = norm.fit(zResidualsUm) #Fit Gaussian
+plt.plot(bins, mlab.normpdf(bins,mu,sigma), 'r--', linewidth=2) #Plot the fit
+plt.title('Fit z residuals : mean=%f sigma=%f : [um]'%(mu,sigma))
+plt.show()
 
