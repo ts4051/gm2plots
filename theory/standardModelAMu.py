e0a80b8c2ae652c3c602b3a5f91022d15ac4808c
Tom Stuttard <ts4051@googlemail.com>
2016-11-29 10:18:05 +0100

    Added plots of a_mu value and error

---
+++ theory/standardModelAMu.py
@@ -0,0 +1,90 @@
+#Plot the standard model values for a_mu and the associated errors
+#Tom Stuttard (29th Nov 2016)
+
+#TODO Add deviation
+#TODO Compare theory and experiment uncertainties
+
+import matplotlib.pyplot as plt
+
+#Define a_mu values and errors for the various contributions
+#Using TDR table 2.3 for values
+labels = list()
+amuValues = list()
+amuErrors = list()
+
+#QED
+labels.append("QED")
+amuValues.append(116584718.951e-11)
+amuErrors.append( ( 0.009 + 0.019 + 0.007 +  0.077 ) * 1.e-11 )
+
+#EW
+labels.append("EW")
+amuValues.append(153.6e-11)
+amuErrors.append( 1.0 * 1.e-11 )
+
+#Hadronic
+labels.append("Hadronic LO")
+amuValues.append( 6923.e-11 )
+amuErrors.append( 42. * 1.e-11 )
+
+labels.append("Hadronic HO")
+amuValues.append( 98.4e-11 )
+amuErrors.append( 0.7 * 1.e-11 )
+
+labels.append("Hadronic LbL")
+amuValues.append( 105.e-11 )
+amuErrors.append( 26. * 1.e-11 )
+
+#Re-calculate amu values in absoliute terms (plotting -ve a bit tricky on a pie chart)
+amuAbsValues = [ abs(v) for v in amuValues ]
+
+#Re-calculate as percentages
+amuAbsValuesSum = sum(amuAbsValues)
+amuAbsValuesPercentages = [ 100. * v / amuAbsValuesSum for v in amuAbsValues]
+
+amuErrorsSum = sum(amuErrors)
+amuErrorsPercentages = [ 100. * e / amuErrorsSum for e in amuErrors]
+
+#Draw pie chart of values
+plt.figure(facecolor='white') #White background
+plt.pie(amuAbsValuesPercentages, explode=([0]*len(labels)), labels=[labels[0],'Others','','',''] )#, autopct='%1.1f%%')
+plt.axis('equal') #Make pie a circle
+plt.show()
+
+#Draw pie chart of errors
+plt.figure(facecolor='white') #White background
+plt.pie(amuErrorsPercentages, explode=([0]*len(labels)), labels=labels )#, autopct='%1.1f%%')
+plt.axis('equal') #Make pie a circle
+plt.show()
+
+'''
+
+fig = plt.figure()
+ax = fig.gca()
+import numpy as np
+
+ax.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
+       autopct='%1.1f%%', shadow=True, startangle=90,
+       radius=0.25, center=(0, 0), frame=True)
+ax.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
+       autopct='%1.1f%%', shadow=True, startangle=90,
+       radius=0.25, center=(1, 1), frame=True)
+ax.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
+       autopct='%1.1f%%', shadow=True, startangle=90,
+       radius=0.25, center=(0, 1), frame=True)
+ax.pie(np.random.random(4), explode=explode, labels=labels, colors=colors,
+       autopct='%1.1f%%', shadow=True, startangle=90,
+       radius=0.25, center=(1, 0), frame=True)
+
+ax.set_xticks([0, 1])
+ax.set_yticks([0, 1])
+ax.set_xticklabels(["Sunny", "Cloudy"])
+ax.set_yticklabels(["Dry", "Rainy"])
+ax.set_xlim((-0.5, 1.5))
+ax.set_ylim((-0.5, 1.5))
+
+# Set aspect ratio to be equal so that pie is drawn as a circle.
+ax.set_aspect('equal')
+
+plt.show()
+'''
---
+++ util/mathtools.py
@@ -0,0 +1,22 @@
+import numpy as np
+import math
+
+#
+# Calculate distance of closest approach of a line to a point
+#
+
+def dca(lineStart,lineEnd,point):
+
+    #d = | ( r0 - r1 ) x ( r0 - r2 ) | / | r2 - r1 |
+    #where r0 is point, r1 is line start and r2 is line end
+    #http://mathworld.wolfram.com/Point-LineDistance3-Dimensional.html
+
+    numerator = np.cross( (point-lineStart), (point-lineEnd) )
+    numerator = math.sqrt( np.dot(numerator,numerator) )
+
+    denominator = lineEnd - lineStart
+    denominator = math.sqrt( np.dot(denominator,denominator) )
+
+    d = numerator / denominator
+
+    return d
